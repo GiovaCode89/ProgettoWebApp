ccb7c8289d386ff220dd44dc1fd476819cad95ba
Giovanni Granatello <g.granatello@reply.it>
2021-10-12 16:39:33 +0200

    Seconda Commit

---
+++ src/main/java/PrimaServlet.java
@@ -0,0 +1,26 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/home")
+public class PrimaServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String user=req.getParameter("id");
+        String pass=req.getParameter("pass");
+
+        if (user.equals("giovanni") && user.equals("ciao"))
+            req.getRequestDispatcher("html/welcome.html").forward(req,resp);}
+
+
+}
